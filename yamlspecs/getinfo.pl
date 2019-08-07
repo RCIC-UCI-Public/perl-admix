06c518c47e7215f46c52eb69345a0d0615d6e6f4
nadyawilliams <nadya.williams@gmail.com>
2019-08-06 18:56:24 -0700

    use MetaCPAN::Client and YAML; MetaCPAN-API is deprecated

--- yamlspecs/getinfo.pl
+++ yamlspecs/getinfo.pl
@@ -1,39 +1,27 @@
-#!/usr/bin/perl
-
-# Usage: perl getinfo.pl ModName|DistName
-#        outputs yaml definition of the package or distribution
-#        that can be parsed  to get info about dependencies
-#        and other values
-# Perl name notation
-#     Aaa::Bbb  for module 
-#     Aaa-Bbb   for distribution
-
 use strict;
 use warnings;
 use 5.010;
  
-use YAML::Dump qw< Dump >;
-use MetaCPAN::API;
+use YAML qw< Dump Bless>;
+use MetaCPAN::Client;
 
-# enforce sorting of the printed object keys
-#use Data::Dumper;
-#$Data::Dumper::Sortkeys = 1;
-  
 my ($name) = @ARGV;
-my $mcpan = MetaCPAN::API->new;
+my $mcpan = MetaCPAN::Client->new;
 my $dist;
 
-# check if the argument was a module name
+# Perl name notation
+# #     Aaa::Bbb  for module 
+# #     Aaa-Bbb   for distribution
+#
+# # check if the argument was a module name
 my $module = eval { $mcpan->module( $name ) };
 if ($@) { # given distribution name
     say STDERR "processing distro $name";
-    $dist = $mcpan->release( distribution => $name );
+    $dist = $mcpan->release( $name );
 }
 else {  # module name
-    say STDERR "processing module $name distro $module->{distribution}";
-    $dist = $mcpan->release( distribution => $module->{distribution} );
+    say STDERR "processing module $name ";
+    $dist = $mcpan->release( $module->distribution );
 }
 
-#say Dumper ($dist);
 say Dump ($dist);
-
