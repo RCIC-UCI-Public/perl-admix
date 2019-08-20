6a75a58ce26ebf9299c1ba3f1e3316c8c16cbf8b
nadyawilliams <nadya.williams@gmail.com>
2019-08-19 18:52:48 -0700

    resolve tracking and ordering all dependencies

--- yamlspecs/depend.py
+++ yamlspecs/depend.py
@@ -7,85 +7,140 @@ import subprocess
 import yaml
 from re import match
 
+class Node(object):
+    def __init__(self, name):
+        self.name = name     # perl name with "::
+        self.edges = []
+    def addEdge(self, node):
+        self.edges.append(node)
+
+    def resolve(self,resolved):
+        for edge in self.edges:
+            if edge not in resolved:
+                edge.resolve(resolved)
+        resolved.append(self)
+
+
 class ModInfo:
     def __init__(self, name):
-        self.perlname = name
-        #self.name = self.perlname.replace("::",'-')
+        self.perlname = name   # perl notation, i.e. Try::Tiny
         self.version = None
         self.download = None
         self.cmd = ['perl', 'getinfo.pl', self.perlname]
+        self.rename = 0  # flag 
 
-        self.readCpanInfo()
+        self.getCpanModInfo()
 
     def printInfo(self):
-        print ("NAME", self.name)
-        #print ("VERSION", self.version)
+        print ("NAME", self.perlname, "parent=%d: %s" % (self.rename,self.mainModule), "Version", self.version)
+        #print ("    PREREQS", self.prereqs)
         #print ("download", self.download)
         #print ("provides", self.provides)
-        print ("depend-config", self.confPrereqs)
-        print ("depend-runtime", self.runPrereqs)
+        #print ("prereqs", self.prereqs)
 
     def getName(self):
-        # first one is regular notatio, secodnis perl. Example: Try-Tiny and Try::Tiny
         return (self.name, self.perlname)
 
-    def getPrereqs(self, ptype):
-        if ptype == "configure":
-            return self.confPrereqs
-        elif ptype == "runtime":
-            return self.runPrereqs
-        else:
-            print ("Error: unknown prerequisite type", ptype)
+    def getPrereqs(self):
+        return self.prereqs
 
-    def readCpanInfo(self):
+    def getRename(self):
+        return (self.rename)
+
+    def getMainMod(self):
+        return (self.mainModule)
+
+    def getVersion(self):
+        return (self.version)
+
+    def getCpanModInfo(self):
         '''Parse the output of the perl command '''
-        print ("workling on ", self.perlname)
+        #print ("DEBUG workling on ", self.perlname)
         p = subprocess.Popen( self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (output, error) = p.communicate()
-        # discard perl "blessed" part, leave only what we need to process
-        firstData = output.find("\ndata:")
-        output = "---" + output[firstData:]
 
-        if error:
-            #TODO
+        # discard perl "blessed" part that has illegal !! in yaml
+        # leave only what we need to process
+        index = output.find("\ndata:")
+        output = "---" + output[index:]
+
+        if error: #TODO
             print (error)
 
         info = yaml.load(output)
         data = info['data']
+
+        # the requested module is provided by another parent main module
+        try: 
+            self.mainModule = data['main_module']
+            if self.mainModule != self.perlname:
+                self.rename = 1
+        except:
+            pass
         
-        self.version     = (data['version'])
-        self.name        = (data["metadata"]['name'])
-        self.provides    = (data['provides'])
-        self.download    = (data['download_url'])
-        self.description = (data['abstract'])
-        prereqs          = data['metadata']['prereqs']
-        self.confPrereqs = prereqs['configure']['requires']
-        self.runPrereqs  = prereqs['runtime']['requires']
+        self.version     = data['version']
+        self.name        = data["metadata"]['name'] # notation where "::" is changed to "-"
+        self.provides    = data['provides']
+        self.download    = data['download_url']
+        self.description = data['abstract']
 
-    def updatePrereqs(self, installed):
-        ''' check the list of installed modules and update prereqs '''
-        instnames = installed.keys()  # names of perl modules currently avail on the system via perl RPMs
+        # some perl packages don't provide prereqs
+        try:
+            prereqs      = data['metadata']['prereqs']
+        except:
+            self.prereqs = {} 
+            return
 
-        # check config prereqs vs installed 
-        for k in self.confPrereqs.keys():
+        try:
+            # from configure phase
+            cPrereqs = prereqs['configure']['requires']
+        except:
+            cPrereqs = {}
+        try:
+            # from runtime phase
+            rPrereqs = prereqs['runtime']['requires']
+        except:
+            rPrereqs = {}
+
+        self.prereqs = cPrereqs.copy()
+        self.prereqs.update(rPrereqs)
+
+    def updatePrereqs(self, syspkgs):
+        ''' check against the list of system modules and update prerequisites '''
+        instnames = syspkgs.keys()  # names of system perl modules currently avail 
+
+        # rm perl from prereqs
+        if 'perl' in self.prereqs.keys():
+            self.prereqs.pop('perl')
+
+        # check module prereqs vs system installed 
+        for k in self.prereqs.keys():
 	    if k in instnames:
-               if self.confPrereqs[k] < installed[k]: # installed version is sufficient
-                   del self.confPrereqs[k]
+               if self.prereqs[k] < syspkgs[k]: 
+                   del self.prereqs[k] # system installed version is sufficient, rm module from prereqs
 
-        # check runtime prereqs vs installed 
-        for k in self.runPrereqs.keys():
-	    if k in instnames:
-               if self.runPrereqs[k] < installed[k]:  # installed version is sufficient
-                   del self.runPrereqs[k]
+    def updatePrereqsMap(self, mmap):
+        ''' update prereqs according to the names in mmap '''
+        mapkeys = mmap.keys()
+        result = {}
+        for k, v in self.prereqs.items():
+            if k in mapkeys:
+                modname, modversion = mmap[k]
+                result[modname] = modversion # use updated name/version
+            else:                  
+                result[k] = v                # use existing name/version
+
+        self.prereqs = result
 
 class BuildDepend:
     def __init__(self, args=None):
         self.args = args
         self.cmd = ['cpan', '-l']
-        self.installed = {}   # default installed modules via perl RPM 
-        self.modlist = []     # list of desired module names, will get from input file 
-        self.dependsConf = {}
-        self.dependsRun = {}
+        self.syspkgs = {}   # default installed system perl modules 
+        self.modnames = []  # list of desired module names, will get from input file 
+        self.desired = {}   # dictionary of desired modules, keys: mod names, values: ModInfo objects 
+        self.depends = {}
+        self.nodes = []     # graph nodes
 
         self.parseArgs()
 
@@ -99,19 +154,20 @@ class BuildDepend:
         return
 
     def exitHelp(self):
-        helpstr = "NAME\n        %s - collect module info\n" % self.prog \
+        helpstr = "NAME\n        %s - create perl modules dependency list \n" % self.prog \
                 + "\nSYNOPSIS\n        %s FILE\n" % self.prog \
                 + "\nDESCRIPTION\n" \
-                + "        Collect information about perl modules installed on the host using 'cpan -l'. \n" \
-                + "        FILE - list of perl modules one per line  \n" \
-                + "        When executing on a command line, outputs collected info on stdout.\n\n" \
+                + "        Collect information about perl system installed modules on the host using 'cpan -l'. \n" \
+                + "        FILE - list of perl modules to install one per line  \n" \
+                + "        For each module name in FILE, get cpan info about the module and build a \n\n" \
+                + "        module dependency ordered list.  \n\n" \
                 + "        -h, --h, --help, help\n              Print usage info.\n\n"
 
         print (helpstr)
         sys.exit(0)
 
-    def listCpanInfo(self):
-        '''Parse the output of the 'cpan -l' command and create a dictionary of installed modules instances'''
+    def getCpanList(self):
+        '''Parse the output of the 'cpan -l' command and create a dictionary of system installed modules '''
         p = subprocess.Popen( self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (output, error) = p.communicate()
 
@@ -125,13 +181,12 @@ class BuildDepend:
                      version = "-1"
                 else:
                      version = items[1]
-                self.installed[items[0]] = version
+                self.syspkgs[items[0]] = version
             else:
                 print ("ERROR", l) # line should have name and version
 
-    def readModFile(self):
+    def getDesiredList(self):
         ''' get a list of desired modules from the file '''
-        self.desired = {}
         if not os.path.isfile(self.fname):
             self.exitHelp()
         f = open(self.fname)
@@ -141,43 +196,123 @@ class BuildDepend:
         for l in lines:
             if l[0] == "#": continue  # skip comment ines
             name = ''.join(l.split()) # rm white spaces
-            self.modlist.append(name)
+            self.modnames.append(name)
 
-    def getModPrereqs(self):
-        ''' for module names form the read file find their prerequisite modules '''
-        for name in self.modlist:
+    def getPrereqs(self, modnames):
+        ''' for desired module names find their prerequisite modules '''
+        for name in modnames:
             mod = ModInfo(name)
-            mod.updatePrereqs(self.installed)    
+            mod.updatePrereqs(self.syspkgs)    
             mod.printInfo()    
             self.desired[name] = mod
-            self.addDepends(mod)
+            self.addPrereqs(mod)
 
-    def addDepends(self, mod):
-        name, perlname = mod.getName()
+    def addPrereqs(self, mod):
+        ''' update overall dependencies by adding found in the mod '''
 
-        # update dependencies for configure stage using listed for mod's
-        dict1 = mod.getPrereqs("configure") # config dependencies from the mod
-        dict2 = self.dependsConf            # current collected config dependencies
-        result = {key: dict1.get(key, 0) if dict1.get(key, 0) > dict2.get(key, 0) else dict2.get(key,0) for key in set(dict1) | set(dict2)}
-        if perlname in result.keys():
-            del result[perlname] # if mod is lsitd in desired, remove from dependencies
-        self.dependsConf = result
+        # combine dependencies so far collected and add any new from the mod
+        # based on higher value for the common keys. If the mod name is found
+        # in dependencies, clear it (already is desired list)
+
+        dict1 = mod.getPrereqs() # dependencies from the mod
+        dict2 = self.depends     # current collected dependencies
+        # this does not work in python2
+        #result = {key: dict1.get(key, 0) if dict1.get(key, 0) > dict2.get(key, 0) else dict2.get(key,0) for key in set(dict1) | set(dict2)}
+        for k,v in dict1.items():
+            if k in self.depends: 
+                if self.depends[k] < v:
+                    self.depends[k] = v  
+            else:
+               self.depends[k] = v
+
+        # find main module name
+        perlname = mod.getMainMod()
+        if perlname in self.depends:
+            del self.depends[perlname]
           
-        # update dependencies for runtime stage using listed for mod's
-        dict1 = mod.getPrereqs("runtime")   # runtime dependencies from the mod
-        dict2 = self.dependsRun             # current collected runtime dependencies
-        result = {key: dict1.get(key, 0) if dict1.get(key, 0) > dict2.get(key, 0) else dict2.get(key,0) for key in set(dict1) | set(dict2)}
-        if perlname in result.keys():
-            del result[perlname] # if mod is lsitd in desired, remove from dependencies
-        self.dependsRun = result
+    def createMap(self):
+        # remap desired modules names according to their main modules 
+        self.namemap = {}
+        modnames = self.desired.keys()
+        result = {}
+        for m in modnames:
+            mod = self.desired[m]
+            if mod.getRename():
+                parent = mod.getMainMod()
+                version = mod.getVersion()
+                self.namemap[m] = (parent, version)
+                result[parent] = mod
+            else:
+                result[m] = mod
 
+        self.desired = result
+
+    def mapPrereqs(self):
+        # remap rereqs names according to the names in map
+        if self.namemap == {}:
+            return
+        
+        for k in self.desired.keys():
+            mod = self.desired[k]
+            mod.updatePrereqsMap(self.namemap)
+
+    def allPrereqs(self):
+        self.getPrereqs(self.modnames)
+        modnames = self.depends.keys()
+        # get cpan info on all prereqs till all are accounted 
+        while len(modnames):
+            self.getPrereqs(modnames)
+            modnames = [x for x in self.depends.keys() if x not in self.desired.keys()]
+
+        # create mapping for renaming modules and rename all prereqs
+        self.createMap()
+        self.mapPrereqs()
+
+    def printInfo(self):
+        for k,v in self.namemap.items():
+            print ("MAP", k, v)
+
+        print ("modnames", len (self.modnames), "prereqs=", len(self.depends), "desired=", len(self.desired)) 
+        for k,v in self.desired.items():
+            print (k, v.getVersion())
+
+    def createNodes(self):
+        for k in self.desired.keys():
+            self.nodes.append(Node(k))
+
+        # make a  master Node to and add all edges to it to make sure that the dependency graph is connected
+        master = Node('root')
+        for node in self.nodes:
+            master.addEdge(node)
+            deps = self.desired[node.name].getPrereqs()
+            print ("for NODE", node.name, deps)
+            try:
+                edges = filter(lambda x: x.name in deps, self.nodes)
+                for edge in edges:
+                    node.addEdge(edge)
+                    print ("    add EDGE", edge.name)
+            except:
+                pass
+
+        # resolve the order
+        resolved = [] 
+        master.resolve(resolved)
+        self.resolved = resolved
+
+    def writeYaml(self):
+        for pkg in self.resolved:
+            print pkg.name
 
     def run(self):
-        self.listCpanInfo()
-        self.readModFile()
-        self.getModPrereqs()
-        print ("total conf prereqs", self.dependsConf)
-        print ("total run prereqs", self.dependsRun)
+        self.getCpanList()
+        self.getDesiredList()
+        self.allPrereqs()
+
+        self.createNodes()
+        self.writeYaml()
+
+        self.printInfo()
+
 
 ##### Run from a command line #####
 if __name__ == "__main__":
